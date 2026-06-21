0d969254bc51d55d30f197751eaf5a2495d85df9
Supraja2410 <jasup.techno@gmail.com>
2026-06-21 17:46:00 +1200

    Modify fruit options query and display dataframe
    Updated the query to include 'SEARCH_ON' column and display the dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,10 @@ name_on_order = st.text_input('Name on Smoothie: ')
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-fruit_list = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+#fruit_list = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
 
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:',
